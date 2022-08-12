c26fa6ef20c30f2e22ddbb4a9736e56aa3b3b068
Daniel <daniel_mesquita2@hotmail.com>
2022-08-12 01:09:04 -0300

    Refatoração do código, separando componentes por função

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -16,19 +16,7 @@
         <section
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <StopwatchDisplay :timeInSeconds="timeInSeconds" />
-          <button class="button" @click="startStopwatch">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="endStopwatch">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
+          <StopwatchControls />
         </section>
       </div>
     </div>
@@ -37,29 +25,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import StopwatchDisplay from "./Stopwatch.vue";
+import StopwatchControls from "./StopwatchControl.vue";
 
 export default defineComponent({
   name: "FormHandler",
   components: {
-    StopwatchDisplay,
-  },
-  //data define estados para o componente
-  data() {
-    return {
-      timeInSeconds: 0,
-      stopWatch: 0, //referência para o intervalo (setInterval), assim podemos utilizar no clearInterval
-    };
-  },
-  methods: {
-    startStopwatch() {
-      this.stopWatch = setInterval(() => {
-        this.timeInSeconds += 1;
-      }, 1000);
-    },
-    endStopwatch() {
-      clearInterval(this.stopWatch);
-    },
+    StopwatchControls,
   },
 });
 </script>
---
+++ src/components/StopwatchControl.vue
@@ -0,0 +1,43 @@
+<template>
+  <StopwatchDisplay :timeInSeconds="timeInSeconds" />
+  <button class="button" @click="startStopwatch">
+    <span class="icon">
+      <i class="fas fa-play"></i>
+    </span>
+    <span>play</span>
+  </button>
+  <button class="button" @click="endStopwatch">
+    <span class="icon">
+      <i class="fas fa-stop"></i>
+    </span>
+    <span>stop</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import StopwatchDisplay from "./Stopwatch.vue";
+
+export default defineComponent({
+  name: "StopwatchControls",
+  components: {
+    StopwatchDisplay,
+  },
+  data() {
+    return {
+      timeInSeconds: 0,
+      stopWatch: 0, //referência para o intervalo (setInterval), assim podemos utilizar no clearInterval
+    };
+  },
+  methods: {
+    startStopwatch() {
+      this.stopWatch = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000);
+    },
+    endStopwatch() {
+      clearInterval(this.stopWatch);
+    },
+  },
+});
+</script>
