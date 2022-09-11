416a5a09836395842d822a381afda05ecbb81eeb
Daniel <daniel_mesquita2@hotmail.com>
2022-09-10 23:54:44 -0300

    feat: activate routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,23 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Tasks from '../views/Tasks.vue'
+import Tasks from "../views/Tasks.vue";
+import ProjectsManager from "../views/Projects.vue";
 
-const routes: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tasks',
-    component: Tasks
-}];
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tasks",
+    component: Tasks,
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: ProjectsManager,
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routes
+  history: createWebHashHistory(),
+  routes: routes,
 });
 
 export default router;
