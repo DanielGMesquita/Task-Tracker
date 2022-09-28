1f99fc8dba2b727b7e69e5f4b72f34cd18f8c489
Daniel <daniel_mesquita2@hotmail.com>
2022-09-28 10:48:38 -0300

    Feat: adding new store

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,7 @@
   <div class="box form">
     <div class="columns">
       <div
-        class="column is-7"
+        class="column is-5"
         role="form"
         area-label="Formulário para criação de uma nova tarefa"
       >
@@ -12,6 +12,20 @@
           placeholder="Qual tarefa você deseja iniciar?"
           v-model="taskDescription"
         />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="projectId">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="project.id"
+              v-for="project in projects"
+              :key="project.id"
+            >
+              {{ project.name }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="column">
         <section
@@ -25,7 +39,10 @@
 </template>
 
 <script lang="ts">
+import { key } from "@/store";
+import { computed } from "@vue/reactivity";
 import { defineComponent } from "vue";
+import { useStore } from "vuex";
 import StopwatchControls from "./StopwatchControl.vue";
 
 export default defineComponent({
@@ -37,6 +54,7 @@ export default defineComponent({
   data() {
     return {
       taskDescription: "",
+      projectId: ""
     };
   },
   methods: {
@@ -44,10 +62,17 @@ export default defineComponent({
       this.$emit("whenSaveTask", {
         durationInSeconds: timeElapsed,
         description: this.taskDescription,
+        project: this.projects.find(proj => proj.id == this.projectId)
       });
       this.taskDescription = "";
     },
   },
+  setup() {
+    const store = useStore(key);
+    return {
+      projects: computed(() => store.state.projects)
+    }
+  }
 });
 </script>
 
